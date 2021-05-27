5d95332014845827307bff27aa0f2026b8bcc2e5
Dylan McDonald <dylan@dlmconsulting.org>
2021-05-27 14:41:20 -0400

    Basic interface, app icon, about screen, etc finished

--- MQSpeedLimitTest/ViewController.swift
+++ MQSpeedLimitTest/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var speedLimitLabel: UILabel!
+    @IBOutlet weak var SLLTrailing: NSLayoutConstraint!
+    @IBOutlet weak var SLLLeading: NSLayoutConstraint!
+    @IBOutlet weak var SLLBottom: NSLayoutConstraint!
+    @IBOutlet weak var SLLHeight: NSLayoutConstraint!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let totalScale = ((mainImage.bounds.width / 326) + (mainImage.bounds.height / 407)) / 2
+        
+        SLLLeading.constant = 70 * (mainImage.bounds.width / 326)
+        SLLTrailing.constant = 70 * (mainImage.bounds.width / 326)
+        SLLBottom.constant = 235 * (mainImage.bounds.height / 407)
+        SLLHeight.constant = 182 * (mainImage.bounds.height / 407)
+        
+        speedLimitLabel.font = speedLimitLabel.font.withSize(190 * totalScale)
     }
-
 
 }
 
